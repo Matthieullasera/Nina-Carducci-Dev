01c927ec8bd83136e474bac80a3dd8e25baf4773
Matthieu_llasera <matthieullasera@gmail.com>
2024-09-10 10:42:46 +0200

    correction of filter background when the filter is active

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -209,47 +209,42 @@
                     </div>
                 </div>
             </div>`);
-    },
+    }, 
     showItemTags(gallery, position, tags) {
       var tagItems =
-        '<li class="nav-item"><span class="nav-link active active-tag"  data-images-toggle="all">Tous</span></li>';
+          '<li class="nav-item"><span class="nav-link active active-tag" data-images-toggle="all">Tous</span></li>';
       $.each(tags, function(index, value) {
-        tagItems += `<li class="nav-item active">
-                <span class="nav-link"  data-images-toggle="${value}">${value}</span></li>`;
+          tagItems += `<li class="nav-item">
+                  <span class="nav-link" data-images-toggle="${value}">${value}</span></li>`;
       });
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
-
       if (position === "bottom") {
-        gallery.append(tagsRow);
+          gallery.append(tagsRow);
       } else if (position === "top") {
-        gallery.prepend(tagsRow);
+          gallery.prepend(tagsRow);
       } else {
-        console.error(`Unknown tags position: ${position}`);
+          console.error(`Unknown tags position: ${position}`);
       }
-    },
-    filterByTag() {
-      if ($(this).hasClass("active-tag")) {
-        return;
-      }
-      $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
-
-      var tag = $(this).data("images-toggle");
-
-      $(".gallery-item").each(function() {
-        $(this)
-          .parents(".item-column")
-          .hide();
-        if (tag === "all") {
-          $(this)
-            .parents(".item-column")
-            .show(300);
-        } else if ($(this).data("gallery-tag") === tag) {
-          $(this)
-            .parents(".item-column")
-            .show(300);
-        }
+      $(".tags-bar .nav-link").on("click", function() {
+          $(".tags-bar .nav-link").removeClass("active active-tag");
+          $(this).addClass("active active-tag");
       });
+  },
+  filterByTag() {
+    if ($(this).hasClass("active-tag")) {
+        return;
     }
-  };
+    $(".active-tag").removeClass("active active-tag");
+    $(this).addClass("active active-tag");
+    var tag = $(this).data("images-toggle");
+    $(".gallery-item").each(function() {
+        $(this).parents(".item-column").hide(); 
+        if (tag === "all") {
+            $(this).parents(".item-column").show(300);
+        } else if ($(this).data("gallery-tag") === tag) {
+            $(this).parents(".item-column").show(300);
+        }
+    });
+  }
+};
 })(jQuery);
